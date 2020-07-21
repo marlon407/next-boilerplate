47e8ad2ff28ee219f7628a8e4744a522e8ea69af
Marlon <marlondiego407@gmail.com>
2020-07-21 20:28:12 -0300

    Adding prettier support

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 
 type Props = {
-  title: string 
+  title: string
 }
 
-export default function Home({ title = "React Avançado" }: Props) {
+export default function Home({ title = 'React Avançado' }: Props) {
   return (
     <div className="container">
       <Head>
@@ -13,9 +13,7 @@ export default function Home({ title = "React Avançado" }: Props) {
       </Head>
 
       <main>
-        <h1 className="title">
-          {title}
-        </h1>
+        <h1 className="title">{title}</h1>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
