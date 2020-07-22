ed916bb9131661fa9a96d18cb640699249ca7f01
Marlon <marlondiego407@gmail.com>
2020-07-22 20:33:43 -0300

    Add styles to out main componente

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1> React Avançado </h1>{' '}
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+    />
+    <S.Title> React Avançado </S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJs e Styled Componentes
+    </S.Description>
+    <S.Ilustration
+      src="/img/hero-illustration.svg"
+      alt="desenvolvedor de frente para o código"
+    />
+  </S.Wrapper>
 )
 
 export default Main
