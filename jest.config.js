82c8c47d0ff8d4e5f6fcdf8ff1717f0516b96706
Marlon <marlondiego407@gmail.com>
2020-07-22 19:55:21 -0300

    Adding findRelatedTests

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collecctCoverageFrom: ['src/**/*.ts(x)'],
+  collectCoverageFrom: ['src/**/*.ts(x)'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 }
